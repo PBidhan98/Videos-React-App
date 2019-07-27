238d255071e41dd929505498a32a27d5a4a1a0c0
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-27 20:32:09 +0530

    reminder on event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {term: ''};
+
+  onInputChange = () => {
+
+  };
+
   render() {
-    return <div>SearchBar</div>
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              //callback handler to the onChange prop will be called anytime input changes
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
