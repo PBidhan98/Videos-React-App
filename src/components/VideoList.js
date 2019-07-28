7a924719029b867862225658a189ffa0f82fac85
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-28 21:05:34 +0530

    Deeply nested callback from App to VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 //functional Component
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
 //returns a brand new array which contains list of video item components
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video}/>;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video}/>;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
