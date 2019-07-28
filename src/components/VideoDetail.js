0fbbdc7f7e19b5893db2d5cef343c51e4de3d64e
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-28 21:33:12 +0530

    Displaying a Video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,10 +4,18 @@ const VideoDetail = ({video}) => {
   if(!video){
     return <div>Loading...</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
-    <div className="ui segment">
-      <h4 className="ui header">{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
     </div>
   );
 };
