87344644927454f25537e3e05bbf596771073c6d
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-28 21:15:28 +0530

    Conditional Rendering & VideoDetail Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
@@ -18,14 +19,18 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("From the App", video);
+    this.setState({selectedVideo: video});
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+            onVideoSelect={this.onVideoSelect}
+            videos={this.state.videos}
+        />
       </div>
     );
   }
