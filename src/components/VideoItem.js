fc12f6f16254b7579cb57f7bbfd317ce24109234
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-28 14:52:05 +0530

    Rendering a list of videos

---
+++ src/components/VideoItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//functional Component
+const VideoItem = (props) => {
+  return <div>video Item</div>;
+};
+
+export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 //functional Component
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({videos}) => {
+//returns a brand new array which contains list of video item components
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
